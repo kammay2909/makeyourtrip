d3c51b592f0528a0a6f2ea0c800e77ff401d9bba
kammay29 <mosesk472774@gmail.com>
2025-07-30 15:46:33 +0200

    code refactor

---
+++ src/main/java/com/example/makeyourtrip/hotel.java
@@ -0,0 +1,10 @@
+package com.example.makeyourtrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hotel {
+    @GetMapping("/hotel")
+    public String getData() {return  "Please book hotel & villas for free" ; }
+}
