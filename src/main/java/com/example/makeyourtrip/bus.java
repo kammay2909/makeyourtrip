64de1f884bf5eb862fe4586e8da9f3960192d6dc
kammay29 <mosesk472774@gmail.com>
2025-08-03 12:02:52 +0200

    added bus endpoint

--- src/main/java/com/example/makeyourtrip/bus.java
+++ src/main/java/com/example/makeyourtrip/bus.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class flight {
-    @GetMapping("/flight")
-    public String getData() {return  "Please book flights indigo ticket with 510% discount" ; }
+public class bus {
+    @GetMapping("/bus")
+    public String getData() {return  "Please book bus indigo ticket with 510% discount" ; }
 }
