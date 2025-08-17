b1c1850f39c731447366f577d5d8107970c95550
kammay29 <mosesk472774@gmail.com>
2025-08-17 18:36:03 +0200

    visa

---
+++ src/main/java/com/example/makeyourtrip/visa.java
@@ -0,0 +1,10 @@
+package com.example.makeyourtrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class visa {
+    @GetMapping("/visa")
+    public String getData() {return  "Please book visaticket with 000% discount" ; }
+}
