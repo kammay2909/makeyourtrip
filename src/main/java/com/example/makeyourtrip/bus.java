be97de78d3bdc6e9f62871eddbf6b34e9efbf9a3
kammay29 <mosesk472774@gmail.com>
2025-08-24 12:01:48 +0200

    becky comment

--- src/main/java/com/example/makeyourtrip/bus.java
+++ src/main/java/com/example/makeyourtrip/bus.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class bus {
     @GetMapping("/bus")
-    public String getData() {return  "Rebekka enjoy the flixbus ride!!" ; }
+    public String getData() {return  "Rebekka Meyen enjoy the flixbus ride !!" ; }
 }
